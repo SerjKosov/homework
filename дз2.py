64e6c1ca3d42b3e37dbf48994479068f07c76dcd
SerjKosov <serj200000@mail.ru>
2025-01-26 18:14:49 +0700

    Update дз2.py

--- дз2.py
+++ дз2.py
@@ -1,84 +1,95 @@
 #Задание 2.8
-#a = int(input("Введите трехзначное число: "))
-#digit = a%10
-#tens = a//10%10
-#hundred = a//100
-#print("Кол-во единиц = ", digit)
-#print("Кол-во десятков = ", tens)
-#print(f"сумма цифр в числе {a} равна {tens+digit+hundred}, \nпроизведение равно {digit*tens*hundred}")
+print("Задание 2.8")
+a = int(input("Введите трехзначное число: "))
+digit = a%10
+tens = a//10%10
+hundred = a//100
+print("Кол-во единиц = ", digit)
+print("Кол-во десятков = ", tens)
+print(f"сумма цифр в числе {a} равна {tens+digit+hundred}, \nпроизведение равно {digit*tens*hundred}")
 
 #Задание 2.9
-# a = int(input("Введите трехзначное число : "))
-# digit = a%10
-# tens = a//10%10
-# hundred = a//100
-# print(digit, tens, hundred, sep='')
-
+print("Задание 2.9")
+a = int(input("Введите трехзначное число : "))
+digit = a%10
+tens = a//10%10
+hundred = a//100
+print(digit, tens, hundred, sep='')
 
 #Задание 2.10
-#a = int(input("Введите трехзначное число : "))
-#digit = a%10
-#tens = a//10%10
-#hundred = a//100
-#print(tens, digit, hundred, sep='')
+print("Задание 2.10")
+a = int(input("Введите трехзначное число : "))
+digit = a%10
+tens = a//10%10
+hundred = a//100
+print(tens, digit, hundred, sep='')
 
 #Задание 2.11
-#a = int(input("Введите трехзначное число : "))
-#digit = a%10
-#tens = a//10%10
-#hundred = a//100
-#print(digit, hundred, tens, sep='')
+print("Задание 2.11")
+a = int(input("Введите трехзначное число : "))
+digit = a%10
+tens = a//10%10
+hundred = a//100
+print(digit, hundred, tens, sep='')
 
 #Задание 2.12
-#a = int(input("Введите трехзначное число : "))
-#digit = a%10
-#tens = a//10%10
-#hundred = a//100
-#print(tens, hundred, digit, sep='')
+print("Задание 2.12")
+a = int(input("Введите трехзначное число : "))
+digit = a%10
+tens = a//10%10
+hundred = a//100
+print(tens, hundred, digit, sep='')
 
 #Задание 2.13
-#a = int(input("Введите трехзначное число : "))
-#digit = a%10
-#tens = a//10%10
-#hundred = a//100
-#print(hundred, digit, tens,  sep='')
+print("Задание 2.13")
+a = int(input("Введите трехзначное число : "))
+digit = a%10
+tens = a//10%10
+hundred = a//100
+print(hundred, digit, tens,  sep='')
 
 #Задание 2.14
-# a = int(input("Введите трехзначное число : "))
-# digit = a%10
-# tens = a//10%10
-# hundred = a//100
-# print(digit, tens, ", ", digit, hundred, ", ", tens, hundred, ", ",
-#      tens, digit, ", ", hundred, digit, ", ", hundred, tens, sep='')
+print("Задание 2.14")
+a = int(input("Введите трехзначное число : "))
+digit = a%10
+tens = a//10%10
+hundred = a//100
+print(digit, tens, ", ", digit, hundred, ", ", tens, hundred, ", ",
+      tens, digit, ", ", hundred, digit, ", ", hundred, tens, sep='')
 
 #Задание 2.15
-# a = int(input("Введите четырехзначное число : "))
-# digit = a%10
-# tens = a//10%10
-# hundred = a//100%10
-# thousand = a//1000
-# print(f'Сумма чисел в цифре {a} равна {digit+tens+hundred+thousand},'
-#      f' произведение чисел = {digit*tens*hundred*thousand}')
+print("Задание 2.15")
+a = int(input("Введите четырехзначное число : "))
+digit = a%10
+tens = a//10%10
+hundred = a//100%10
+thousand = a//1000
+print(f'Сумма чисел в цифре {a} равна {digit+tens+hundred+thousand},'
+      f' произведение чисел = {digit*tens*hundred*thousand}')
 
 #Задание 2.16
-# a = int(input("Введите четырехзначное число : "))
-# digit = a%10
-# tens = a//10%10
-# hundred = a//100%10
-# thousand = a//1000
-# print(hundred*1000 + thousand*100 + digit * 10 + tens)
-# print(thousand*1000+tens*100+hundred*10+digit)
-# print(tens*1000+digit*100+thousand*10+hundred)
+print("Задание 2.16")
+a = int(input("Введите четырехзначное число : "))
+digit = a%10
+tens = a//10%10
+hundred = a//100%10
+thousand = a//1000
+print(hundred*1000 + thousand*100 + digit * 10 + tens)
+print(thousand*1000+tens*100+hundred*10+digit)
+print(tens*1000+digit*100+thousand*10+hundred)
 
 #Задание 2.17
-#a = int(input("Введите двухзначное число : "))
-#print(f'В числе {a} единиц: {a%10}, десятков: {a//10%10}')
+print("Задание 2.17")
+a = int(input("Введите двухзначное число : "))
+print(f'В числе {a} единиц: {a%10}, десятков: {a//10%10}')
 
 #Задание 2.18
-# a = int(input("Введите трехзначное число : "))
-# print(f'В числе {a} десятков {a//10%10}, сотен {a//100%10}')
+print("Задание 2.18")
+a = int(input("Введите трехзначное число : "))
+print(f'В числе {a} десятков {a//10%10}, сотен {a//100%10}')
 
 #Задание 2.19
+print("Задание 2.19")
 a = int(input("Введите четырехзначное число : "))
 hundred = a//100%10
 thousand = a//1000%10
