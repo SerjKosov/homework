f5ba34a8dea0d1af88dc90941ad13770f376a05b
SerjKosov <serj200000@mail.ru>
2025-01-26 18:10:20 +0700

    Update дз1.py

--- дз1.py
+++ дз1.py
@@ -1,133 +1,126 @@
 #Задание 1
-# print("Задание 1.1")
-# a = int(input("а = "))
-# b = int(input("b = "))
-# c = int(input("c = "))
-# summ = a+b+c
-# multi = a*b*c
-# print("сумма = ", summ, "\nпроизведение = ", multi)
-#
-#
-# print("Задание 1.2")
-# a = int(input("а = "))
-# b = int(input("b = "))
-# c = int(input("c = "))
-# print("сумма =", format(a+b+c), "\nпроизведение = ", format(a*b*c))
-#
-# print("Задание 1.3")
-#a = int(input("а = "))
-#b = int(input("b = "))
-#c = int(input("c = "))
-#print(f'сумма = {a+b+c}, \nпроизведение = {a*b*c}')
+print("Задание 1.1")
+a = int(input("а = "))
+b = int(input("b = "))
+c = int(input("c = "))
+summ = a+b+c
+multi = a*b*c
+print("сумма = ", summ, "\nпроизведение = ", multi)
 
+print("Задание 1.2")
+a = int(input("а = "))
+b = int(input("b = "))
+c = int(input("c = "))
+print("сумма =", format(a+b+c), "\nпроизведение = ", format(a*b*c))
+print("Задание 1.3")
+
+print("Задание 1.3")
+a = int(input("а = "))
+b = int(input("b = "))
+c = int(input("c = "))
+print(f'сумма = {a+b+c}, \nпроизведение = {a*b*c}')
 
 #задание 2
-#print("Задание 2.1")
-#saisry = int(input("З.п. за месяц = "))
-#credit = int(input("Месячный платеж = "))
-#unility =int(input("Коммунальные платежи = "))
-#total_summ = saisry - credit - unility
-#print("осталось на жизнь = ", total_summ)
+print("Задание 2.1")
+saisry = int(input("З.п. за месяц = "))
+credit = int(input("Месячный платеж = "))
+unility =int(input("Коммунальные платежи = "))
+total_summ = saisry - credit - unility
+print("осталось на жизнь = ", total_summ)
 
-#print("Задание 2.2")
-#saisry = int(input("З.п. за месяц = "))
-#credit = int(input("Месячный платеж = "))
-#unility =int(input("Коммунальные платежи = "))
-#print("Осталось на жизнь = ","{}".format(saisry-credit-unility))
+print("Задание 2.2")
+saisry = int(input("З.п. за месяц = "))
+credit = int(input("Месячный платеж = "))
+unility =int(input("Коммунальные платежи = "))
+print("Осталось на жизнь = ","{}".format(saisry-credit-unility))
 
-#print("Задание 2.3")
-#saisry = int(input("З.п. за месяц = "))
-#credit = int(input("Месячный платеж = "))
-#unility =int(input("Коммунальные платежи = "))
-#print(f"Осталось на жизнь = {saisry-credit-unility}")
+print("Задание 2.3")
+saisry = int(input("З.п. за месяц = "))
+credit = int(input("Месячный платеж = "))
+unility =int(input("Коммунальные платежи = "))
+print(f"Осталось на жизнь = {saisry-credit-unility}")
 
 #Задание 3
-#print("Задание 3.1")
-#a = int(input("Введите дигональ а= "))
-#b = int(input("Введите диагональ b= "))
-#s = a*b/2
-#print("Площадь ромба = ", s)
+print("Задание 3.1")
+a = int(input("Введите дигональ а= "))
+b = int(input("Введите диагональ b= "))
+s = a*b/2
+print("Площадь ромба = ", s)
 
-#print("Задание 3.2")
-#a = int(input("Введите дигональ а= "))
-#b = int(input("Введите диагональ b= "))
-#print("Площадь ромба = ", '{}'.format(a*b/2))
+print("Задание 3.2")
+a = int(input("Введите дигональ а= "))
+b = int(input("Введите диагональ b= "))
+print("Площадь ромба = ", '{}'.format(a*b/2))
 
-#print("Задание 3.3")
-#a = int(input("Введите дигональ а= "))
-#b = int(input("Введите диагональ b= "))
-#print(f"площадь ромба = {a*b/2}")
-
+print("Задание 3.3")
+a = int(input("Введите дигональ а= "))
+b = int(input("Введите диагональ b= "))
+print(f"площадь ромба = {a*b/2}")
 
 #Задание 4
-#print("Задание 4")
-#print("To be \nor not \nto be")
+print("Задание 4")
+print("To be \nor not \nto be")
 
 #Задание 5
-#print("Задание 5")
-#print( '"Life is what happens \n\twhen \n\t\tyou\'re busy making other plans" \n\t\t\t\t\t\tJohn Lennon'  )
-
+print("Задание 5")
+print( '"Life is what happens \n\twhen \n\t\tyou\'re busy making other plans" \n\t\t\t\t\t\tJohn Lennon'  )
 
 #Задание 6
-#print("Задание 6")
-#metr = float(input("Количество метров = " ))
-#santy = metr*100
-#deci = metr*10
-#milli = metr*1000
-#mily = metr*0.62e-3
-#print(" В сантиметрах = ", santy, "см.\n", "В дециметрах = ", deci, "дм.\n", "В миллиметрах = ", milli, "мм.\n", "В милях = ", mily, "мили")
+print("Задание 6")
+metr = float(input("Количество метров = " ))
+santy = metr*100
+deci = metr*10
+milli = metr*1000
+mily = metr*0.62e-3
+print(" В сантиметрах = ", santy, "см.\n", "В дециметрах = ", deci, "дм.\n", "В миллиметрах = ", milli, "мм.\n", "В милях = ", mily, "мили")
 
 
 #Задание 7
-#print("Задание 7")
-#a = int(input("Введите а = "))
-#b = int(input("Введите b = "))
-#c = format( a/b, '.2f' )
-#print(c)
-
-
+print("Задание 7")
+a = int(input("Введите а = "))
+b = int(input("Введите b = "))
+c = format( a/b, '.2f' )
+print(c)
 
 #Задание 11
-#print("Задание 11")
-#a = input()
-#b = input()
-#a,b = b,a
-#print(a,b)
-
+print("Задание 11")
+a = input()
+b = input()
+a,b = b,a
+print(a,b)
 
 #Задание 12
-#print("Задание 12")
-#a = int(input("Введите а = "))
-#b = int(input("Введите b = "))
-#c = int(input("Введите c = "))
-#a, b, c = c, a, b
-#print(a, b, c)
+print("Задание 12")
+a = int(input("Введите а = "))
+b = int(input("Введите b = "))
+c = int(input("Введите c = "))
+a, b, c = c, a, b
+print(a, b, c)
 
 
 #Задание 13
-#print("Задание 13")
-#a = int(input("Введите длину стороны квадрата = "))
-#square_kv = a**2
-#print("площадь квадрата = ", square_kv)
+print("Задание 13")
+a = int(input("Введите длину стороны квадрата = "))
+square_kv = a**2
+print("площадь квадрата = ", square_kv)
 
-#r = int(input("Введите радиус = "))
-#square_kr = 3.14*r**2
-#print("площадь круга = ", square_kr)
-
-
-#a = int(input("Введите длину стороны квадрата = "))                                   #один вариант
-#print(f'Площадь квадрата = {a**2}')
-
-#r = int(input("Введите радиус = "))
-#print(f'Площадь круга = {3.14*r**2}')
-
-#print(f'Площадь квадрата = {int(input("Введите длину стороны квадрата = "))**2}')     #другой вариант
-
-#print(f'Площадь круга = {int(input("Введите радиус = "))**2*3.14}')
+r = int(input("Введите радиус = "))
+square_kr = 3.14*r**2
+print("площадь круга = ", square_kr)
 
 
-#a = int(input("Введите длину стороны квадрата = "))
-#print("Площадь квадрата = ", '{}'.format(a**2))
+a = int(input("Введите длину стороны квадрата = "))                                   #один вариант
+print(f'Площадь квадрата = {a**2}')
 
-#r = int(input("Введите радиус = "))
-#print("Площадь круга = ", '{}'.format(3.14*r**2))
+r = int(input("Введите радиус = "))
+print(f'Площадь круга = {3.14*r**2}')
+
+print(f'Площадь квадрата = {int(input("Введите длину стороны квадрата = "))**2}')     #другой вариант
+print(f'Площадь круга = {int(input("Введите радиус = "))**2*3.14}')
+
+
+a = int(input("Введите длину стороны квадрата = "))
+print("Площадь квадрата = ", '{}'.format(a**2))
+
+r = int(input("Введите радиус = "))
+print("Площадь круга = ", '{}'.format(3.14*r**2))
